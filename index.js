8e8ee05eb0ba13b31f34a814330280e2701f08e6
Bee K <beetimewa@gmail.com>
2021-08-18 10:53:30 -0700

    a

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+ar express = require('express');
+
+var app = express();
+const PORT = 3000;
+
+app.set('view engine','ejs');//sent views to EJS
+
+//home page
+app.get('/', (req,res)=>{
+  var title = "Our Home Page";
+  res.render('index',{title:title});
+});
+
+app.listen(PORT,()=>{
+  console.log(`App is running on port ${PORT}`);
+  console.log(data);
+
+//add users route
+app.get('/users', function(req, res) {
+	var title = 'Users Page';
+	res.render('users/index', {
+    	title: title,
+    	users: data
+	});
+
+  //user/view route - we are cheating by using the array index + 1
+app.get('/users/view/:id', function(req, res) {
+  var title = 'User Page';
+  var id = req.params.id;
+  res.render('users/view', {
+      title: title,
+      user: data[--id]
+  });
+ });
+ 
+});
+
+});
